c8a49054936d51dd73633b0455f3538a8dcf700c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-24 13:51:49 +0700

    Add MobileNav component to Layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,11 @@
 import Sidebar from '@/components/layout/Sidebar'
+import MobileNav from '@/components/layout/MobileNav'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<main className='root'>
 			<Sidebar />
+			<MobileNav />
 			<div className='root-container'>
 				<div className='wrapper'>{children}</div>
 			</div>
