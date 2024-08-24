5261984180ce9d50bc6125f43a5c8e81c4281bd0
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-24 11:57:00 +0700

    Move ClerkProvider to Rootlayout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,27 +1,9 @@
-import {
-	ClerkProvider,
-	SignedIn,
-	SignedOut,
-	SignInButton,
-	UserButton,
-} from '@clerk/nextjs'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<ClerkProvider>
-			<main className='root'>
-				<div className='root-container'>
-					<div className='wrapper'>
-						<SignedOut>
-							<SignInButton />
-						</SignedOut>
-						<SignedIn>
-							<UserButton />
-						</SignedIn>
-						{children}
-					</div>
-				</div>
-			</main>
-		</ClerkProvider>
+		<main className='root'>
+			<div className='root-container'>
+				<div className='wrapper'>{children}</div>
+			</div>
+		</main>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next'
 import { IBM_Plex_Sans } from 'next/font/google'
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
+} from '@clerk/nextjs'
 
 import './globals.css'
 import { cn } from '@/lib/utils'
@@ -20,10 +27,18 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
-			<body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
+					<SignedOut>
+						<SignInButton />
+					</SignedOut>
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	)
 }
