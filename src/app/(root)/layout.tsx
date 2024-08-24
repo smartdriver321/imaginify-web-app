69ead71fd040ba76c945268e3a90f843cbb76817
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-24 11:48:17 +0700

    Configure Clerk authentication

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,9 +1,27 @@
+import {
+	ClerkProvider,
+	SignedIn,
+	SignedOut,
+	SignInButton,
+	UserButton,
+} from '@clerk/nextjs'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<main className='root'>
-			<div className='root-container'>
-				<div className='wrapper'>{children}</div>
-			</div>
-		</main>
+		<ClerkProvider>
+			<main className='root'>
+				<div className='root-container'>
+					<div className='wrapper'>
+						<SignedOut>
+							<SignInButton />
+						</SignedOut>
+						<SignedIn>
+							<UserButton />
+						</SignedIn>
+						{children}
+					</div>
+				</div>
+			</main>
+		</ClerkProvider>
 	)
 }
