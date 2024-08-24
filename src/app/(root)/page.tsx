fa84116610bb0424b7bbefe79817ac7ee48b4c7e
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-24 13:29:35 +0700

    Refactor authentication

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
 	return (
 		<div>
 			<p>Home</p>
+			<UserButton />
 		</div>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next'
 import { IBM_Plex_Sans } from 'next/font/google'
-import {
-	ClerkProvider,
-	SignedIn,
-	SignedOut,
-	SignInButton,
-	UserButton,
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 import { cn } from '@/lib/utils'
@@ -27,15 +21,13 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				variables: { colorPrimary: '#624cf5' },
+			}}
+		>
 			<html lang='en'>
 				<body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
-					<SignedOut>
-						<SignInButton />
-					</SignedOut>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
 					{children}
 				</body>
 			</html>
