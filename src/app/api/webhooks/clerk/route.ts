e6271ff709239ced036493d96838f4902681dea0
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-25 14:00:24 +0700

    Fixing deployment errors

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,5 +1,4 @@
 /* eslint-disable camelcase */
-import clerkClient from '@clerk/nextjs'
 import { WebhookEvent } from '@clerk/nextjs/server'
 import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
